7a9c427cfabacd0da1884e43b77758dd06a83bc8
ismaiiil <ismaiiil@live.com>
2021-05-10 09:17:43 +0400

    Initial commit

---
+++ src/main/java/com/supinfo/supcrypto/Main.java
@@ -0,0 +1,10 @@
+package com.supinfo.supcrypto;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        while(true){
+
+        }
+    }
+}
